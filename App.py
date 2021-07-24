dfff4353fb0ff1ce4136003a8ff9078c0271e4df
Blackwings123 <maitkduy@gmail.com>
2021-07-24 21:50:20 +0700

    Deadline da xong cua Suneee

--- App.py
+++ App.py
@@ -3,6 +3,12 @@ from tkinter import messagebox
 from Client import *
 import tkinter as tk
 import socket
+from tkinter import filedialog
+import tkinter
+from PIL import ImageTk,Image
+from tkinter import ttk
+from tkinter import filedialog
+global folder_path
 
 HOST = '127.0.0.1'
 PORT = 1233
@@ -24,12 +30,14 @@ def NewWindow():
         connected = messagebox.showinfo("Kết nối", "IP sai")
 def AppRunning():
     newWin = Tk()
-    newWin.geometry("450x300")
     newWin.title("listApp")
 
+    def PressXoa():
+        table.destroy()
     def PressXem():
         global HOST
         global PORT
+        global table
         size = 0
         list_id = [''] * 100
         list_name = [''] * 100
@@ -37,31 +45,31 @@ def AppRunning():
         client.sendall(bytes("Xem App","utf8"))
         check = client.recv(1024).decode("utf8")
         size, list_id, list_name, list_thread = Recieve_App_Running(client, HOST, PORT)
-        text = Label(
-            newWin,
-            text="Name Application"
-            ).grid(row=1,column=0)
-        text = Label(
-            newWin,
-            text="ID Application"
-            ).grid(row=1,column=1)
-        text = Label(
-            newWin,
-            text="Thread Count"
-            ).grid(row=1,column=2)
+
+        table = Frame(newWin, padx=20, pady = 20, borderwidth=5)
+        table.grid(row=1,columnspan=5,padx=20)
+        from tkinter import ttk
+
+        tree_scroll = Scrollbar(table)
+        tree_scroll.pack(side=RIGHT,fill=Y)
+
+        my_tree=ttk.Treeview(table, yscrollcommand=tree_scroll.set)
+        my_tree.pack()
+
+        tree_scroll.config(command=my_tree.yview)
+
+        my_tree['columns'] = ("1","2") 
+        my_tree.column("#0", anchor=CENTER, width =200,minwidth=25)
+        my_tree.column("1", anchor=CENTER, width=60)
+        my_tree.column("2", anchor=CENTER, width=100)
+
+        my_tree.heading("#0", text="Name Process", anchor=W)
+        my_tree.heading("1",text = "ID", anchor=CENTER)
+        my_tree.heading("2", text = "Thread count", anchor=CENTER)
+
         for i in range(size):
-            text = Label(
-                    newWin,
-                    text = list_id[i]
-                ).grid(row = 2+i, column = 1)
-            text = Label(
-                    newWin,
-                    text = list_name[i]
-                ).grid(row = 2+i, column = 0)
-            text = Label(
-                    newWin,
-                    text = list_thread[i]
-                ).grid(row=2+i,column = 2)
+            my_tree.insert(parent='', index='end',iid=0+i, text = list_name[i], values=(list_id[i],list_thread[i]))
+
     def PressKill():
         newWin2 = Tk()
         newWin2.geometry("300x50")
@@ -126,28 +134,28 @@ def AppRunning():
                 padx = 20,
                 command = PressStart
             ).grid(row=0, column=4, padx=5, pady=5)
-
     kill = Button(
         newWin,
         text = "Kill",
         padx = 30, 
         pady = 20,
         command= PressKill
-    ).grid(row = 0, column = 0, padx = 10, pady = 10)
+    ).grid(row = 0, column = 0, padx = 10)
     Xem = Button(
         newWin,
         text = "Xem",
         padx = 30, 
         pady = 20,
         command = PressXem
-    ).grid(row = 0, column = 1, padx = 10, pady = 10)
+    ).grid(row = 0, column = 1, padx = 10)
     
     Xoa = Button(
         newWin,
         text =  "Xóa",
         padx = 30, 
-        pady = 20
-    ).grid(row = 0, column = 2, padx = 10, pady = 10)
+        pady = 20,
+        command = PressXoa
+    ).grid(row = 0, column = 2, padx = 10)
 
     Start = Button(
         newWin,
@@ -155,41 +163,345 @@ def AppRunning():
         padx = 30, 
         pady = 20,
         command = PressStart
-    ).grid(row = 0, column = 3, padx = 10, pady = 10)
-
+    ).grid(row = 0, column = 3, padx = 10)
 def ProcessRunning():
-    newApp = Tk()
-    newApp.geometry("500x300")
-    newApp.title("Close")
-    newApp = Label(newApp, text = "hi").grid(row = 0, column = 0)
+    newWin = Tk()
+    #newWin.geometry("445x300")
+    newWin.title("List Process Running")
+    table = Frame(newWin, padx=20, pady = 20, borderwidth=5)
+    table.grid(row=1,columnspan=5,padx=20)
+    def PressXoaProcess():
+        table.destroy()
+    def PressXemProcess():
+        nonlocal table
+        size = 0
+        list_id = [''] * 1000
+        list_name = [''] * 1000
+        list_thread = [''] * 1000
+        client.sendall(bytes("Xem Process","utf8"))
+        check = client.recv(1024).decode("utf8")
 
+        size, list_id, list_name, list_thread = Recieve_Process_Running(client, HOST, PORT)
+
+        table = Frame(newWin, padx=20, pady = 20, borderwidth=5)
+        table.grid(row=1,columnspan=5,padx=20)
+        
+        from tkinter import ttk
+
+        tree_scroll = Scrollbar(table)
+        tree_scroll.pack(side=RIGHT,fill=Y)
+
+        my_tree=ttk.Treeview(table, yscrollcommand=tree_scroll.set)
+        my_tree.pack()
+
+        tree_scroll.config(command=my_tree.yview)
+
+        my_tree['columns'] = ("1","2") 
+        my_tree.column("#0", anchor=CENTER, width =200,minwidth=25)
+        my_tree.column("1", anchor=CENTER, width=60)
+        my_tree.column("2", anchor=CENTER, width=100)
+
+        my_tree.heading("#0", text="Name Process", anchor=W)
+        my_tree.heading("1",text = "ID", anchor=CENTER)
+        my_tree.heading("2", text = "Thread count", anchor=CENTER)
+
+        for i in range(size):
+            my_tree.insert(parent='', index='end',iid=0+i, text = list_name[i], values=(list_id[i],list_thread[i]))
+
+        
+    def PressKillProcess():
+        newWin2 = Tk()
+        newWin2.geometry("300x50")
+        newWin2.title("Kill")
+        enterID = Entry(
+                newWin2,
+                width = 35
+            )
+        enterID.grid(
+                row=0,
+                column=0, 
+                columnspan = 3,
+                padx = 5,
+                pady = 5 
+            )
+        enterID.insert(END,"Nhập ID")
+        def PressKill2Process():
+            ID = enterID.get()
+            client.sendall(bytes("Xoa Process","utf8"))
+            try:
+                check = client.recv(1024).decode("utf8")
+                client.sendall(bytes(ID,"utf8"))
+                click = messagebox.showinfo("", "Đã diệt chương trình")
+            except:
+                click = messagebox.showinfo("", "Không tìm thấy chương trình")
+
+        bKill = Button(
+                newWin2,
+                text = "Kill",
+                padx = 20,
+                command = PressKill2Process
+            ).grid(row=0, column=4, padx=5, pady=5)
+    def PressStartProcess():
+        newWin3 = Tk()
+        newWin3.geometry("300x50")
+        newWin3.title("Start")
+        enterName = Entry(
+                newWin3,
+                width = 35
+            )
+        enterName.grid(
+                row=0,
+                column=0, 
+                columnspan = 3,
+                padx = 5,
+                pady = 5 
+            )
+        enterName.insert(END,"Nhập Tên")
+        def PressStartProcess():
+            Name = enterName.get()
+            client.sendall(bytes("Bat Process","utf8"))
+            try:
+                check = client.recv(1024).decode("utf8")
+                client.sendall(bytes(Name,"utf8"))
+                click = messagebox.showinfo("", "Chương trình đã bật")
+            except:
+                click = messagebox.showinfo("", "Không tìm thấy chương trình")
+
+        bStart = Button(
+                newWin3,
+                text = "Start",
+                padx = 20,
+                command = PressStartProcess
+            ).grid(row=0, column=4, padx=5, pady=5)
+
+    kill = Button(
+        newWin,
+        text = "Kill",
+        padx = 30, 
+        pady = 20,
+        command= PressKillProcess
+        ).grid(row = 0, column = 0, padx = 10)
+    Xem = Button(
+        newWin,
+        text = "Xem",
+        padx = 30, 
+        pady = 20,
+        command = PressXemProcess
+        ).grid(row = 0, column = 1, padx = 10)
+    
+    Xoa = Button(
+        newWin,
+        text =  "Xóa",
+        padx = 30, 
+        pady = 20,
+        command = PressXoaProcess
+        ).grid(row = 0, column = 2, padx = 10)
+
+    Start = Button(
+        newWin,
+        text="Start",
+        padx = 30, 
+        pady = 20,
+        command = PressStartProcess
+    ).grid(row = 0, column = 3, padx = 10)
 
 def Close():
-    connect = messagebox.askyesno("tat may", "Ban muon tat may")
+    connect = messagebox.askyesno("Tắt máy", "Bạn có muốn tắt máy")
     if connect == 1:
         client.sendall(bytes("Shutdown","utf8"))
     else:
         return
         
 def PrintScreen(): 
-    newApp = Tk()
-    newApp.geometry("500x300")
+    newApp = Toplevel()
     newApp.title("PrintScreen")
-    newApp = Label(newApp, text = "hi").grid(row = 0, column = 0)
+
+    def Chup():
+        client.sendall(bytes("Chup man hinh","utf8"))
+        check = client.recv(1024).decode("utf8")
+
+        myfile = open("anhcuatui.png", 'wb')
+        data = client.recv(40960000)
+
+        myfile.write(data)
+
+        img = ImageTk.PhotoImage(Image.open("anhcuatui.png"))     
+        canvas.create_image(0,0, anchor=NW, image=img)
+        mainloop()
+
+        myfile.close()
+
+    def savefile():
+        myScreenshot = open("anhcuatui.png",'rb')
+        data = myScreenshot.read()
+        fname = tkinter.filedialog.asksaveasfilename(title=u'Save file', filetypes=[("PNG", ".png")])
+        myScreenshot.close()
+
+        file = open(str(fname) + '.png','wb')
+        file.write(data)
+        file.close()
+
+    canvas = Canvas(newApp, width = 500, height = 400)      
+    canvas.grid(row=0,column=0)    
+
+    but = Button(newApp,text="Chụp",width=5,height=10,borderwidth=5,command = Chup)
+    but.grid(row=0,column=1)
+
+    but1 = Button(newApp,text="Lưu",width=5,height=5,borderwidth=5,command=savefile)
+    but1.grid(row=1,column=1)
+
 def Registry(): 
-    newApp = Tk()
-    newApp.geometry("500x300")
-    newApp.title("Registry")
-    newApp = Label(newApp, text = "hi").grid(row = 0, column = 0)
+    newapp = Tk()
+
+    newapp.geometry("500x400")
+
+    newapp.title("registry")
+
+    linkfile = Entry(newapp, width=55)
+    linkfile.grid(row=0, column=0, padx = 10)
+
+    def browse_button():
+        filename = filedialog.askdirectory()
+        folder_path.set(filename)
+        linkfile.insert(0, filename)
+
+    folder_path = StringVar()
+
+    linkfile = Entry(newapp, width=55)
+    linkfile.grid(row=0, column=0, padx = 10)
+
+    Browser = Button(newapp, text="Browser...", command=browse_button, padx = 28)
+    Browser.grid(row=0, column=1, padx = 10)    
+     
+    txt = Entry(newapp)
+    txt.grid(row=2, column=0, pady=10, ipady=30, ipadx=105)
+
+    GuiNoiDung = Button(newapp, text="Gui noi dung", padx = 20, pady = 28)
+    GuiNoiDung.grid(row=2, column=1, padx = 10)
+
+    frame = LabelFrame(newapp, text="Sua gia tri truc tiep")
+    frame.grid(row=3, columnspan = 4, padx = 0, pady = 0)
+
+    option = [
+            "Get value",
+            "Set value",
+            "Delete value",
+            "Create key",
+            "Delete key"
+    ]
+
+    option2 = [
+            "String",
+            "Binary",
+            "DWORD",
+            "QWORD",
+            "Multi-string",
+            "Expandable String"
+    ]
+
+    def show(event):
+        if SetValue.get() == "Get value":
+            NameVal.grid_forget()
+            Value.grid_forget()
+            DuLieu.grid_forget()
+
+            NameVal.grid(row=2, column=0, sticky = W)
+
+        elif SetValue.get() == "Set value":
+            NameVal.grid_forget()
+            Value.grid_forget()
+            DuLieu.grid_forget()
+            
+            NameVal.grid(row=2, column=0, sticky = W)
+            Value.grid(row=2, column=0, sticky = N)
+            DuLieu.grid(row=2, column=0, sticky = E, padx=4)
+
+        elif SetValue.get() == "Delete value":
+            NameVal.grid_forget()
+            Value.grid_forget()
+            DuLieu.grid_forget()
+
+            NameVal.grid(row=2, column=0, sticky = W)
+        elif SetValue.get() == "Create key":
+            NameVal.grid_forget()
+            Value.grid_forget()
+            DuLieu.grid_forget()
+
+
+    SetValue = ttk.Combobox(frame, value=option)
+    SetValue.insert(0, "Chọn chức năng")
+    SetValue.bind("<<ComboboxSelected>>", show)
+    SetValue.grid(row=0,column=0,ipadx=160, sticky=W)
+
+    Đuongdan = Entry(frame, width=77)
+    Đuongdan.insert(0, "Đường dẫn")
+    Đuongdan.grid(row=1, column=0, pady=10)
+
+    NameVal = Entry(frame, width = 24)
+    NameVal.insert(0, "Name value")
+    NameVal.grid(row=2, column=0, sticky = W)
+
+    Value = Entry(frame, width = 25)
+    Value.insert(0, "Value")
+    Value.grid(row=2, column=0, sticky = N)
+
+    DuLieu = ttk.Combobox(frame, value=option2)
+    DuLieu.insert(0, "Kiểu dữ liệu")
+    DuLieu.grid(row=2, column=0, sticky = E, padx=4)
+
+    txt = Entry(frame)
+    txt.grid(row=3, column=0, pady=10, ipady=30, ipadx=172)
+
+    Gui = Button(frame, text="xẻ")
+    Gui.grid(row=4, column=0, sticky=SW, ipadx = 30)
+
+    Xoa = Button(frame, text="mổ")
+    Xoa.grid(row=4, column=0, sticky=NE,ipadx = 30)
+
+    newapp.mainloop()
+
+
 def Keystroke():
+    size = 0
+    string = ''
     newApp = Tk()
-    newApp.geometry("500x300")
-    newApp.title("Keystroke")
-    newApp = Label(newApp, text = "hi").grid(row = 0, column = 0)
+    #newApp.geometry("500x300")
+    newApp.title("Keystroke")  
+    e = Entry(newApp, width = 55)
+    e.grid(row = 1, column = 0,columnspan= 4)
+    unhook_press = False
+    def hook():
+        nonlocal unhook_press
+        unhook_press = False
+        client.sendall(bytes("Hook Key","utf8"))
+        check = client.recv(1024).decode("utf8")
+    def unhook():
+        nonlocal size, string, unhook_press
+        if unhook_press == False:
+            client.sendall(bytes("Unhook Key","utf8"))
+            size , string = Recieve_Hook(client, HOST, PORT)
+            unhook_press = True
+    def xem():
+        nonlocal size, string, unhook_press
+        if unhook_press == False: 
+            client.sendall(bytes("Unhook Key","utf8"))
+            size , string = Recieve_Hook(client, HOST, PORT)
+            unhook_press = True
+        e.delete(0,END)
+        e.insert(0,string)
+    def xoa():
+        e.delete(0,END)
+
+
+    Hook = Button(newApp, text = "Hook", padx = 20, pady = 20, command = hook).grid(row = 0,column = 0)
+    unHook = Button(newApp, text = "Unhook", padx = 20, pady = 20, command = unhook).grid(row = 0,column = 1) 
+    inphim = Button(newApp, text = "In phím", padx = 20, pady = 20,command = xem).grid(row = 0,column = 2)
+    xoa = Button(newApp, text = "Xóa", padx = 20, pady = 20,command=xoa).grid(row = 0,column = 3)
+
 def Quit():
     click = messagebox.askyesno("Quit?", "Bạn chắc chắn muốn thoát")
     if click == 1:
-        app.quit()
+        app.destroy()
     else :
         return 0
 
@@ -237,7 +549,6 @@ Close = Button(
     pady = 22, 
     command=Close
     ).grid(row = 2, column = 1)
-
 PrintScreen = Button(
     app, 
     text="Chụp màn hình", 
@@ -245,7 +556,6 @@ PrintScreen = Button(
     pady = 22, 
     command=PrintScreen
     ).grid(row = 2, column = 2)
-
 Registry = Button(
     app, 
     text="Sửa Registry", 
@@ -253,7 +563,6 @@ Registry = Button(
     pady = 22, 
     command=Registry
     ).grid(row = 3, column = 1, columnspan = 2)
-
 Keystroke = Button(
     app, 
     text="Keystroke", 
@@ -261,7 +570,6 @@ Keystroke = Button(
     pady = 60, 
     command=Keystroke
     ).grid(row = 1, column = 3, rowspan = 2)
-
 Quit = Button(
     app, 
     text="Thoát",
